6526ace86d4fd5de1ec5b742a9b28a2af382926a
Andre Della Libera Zanchetta <adlzanchetta@gmail.com>
2021-07-13 19:23:11 -0400

    Reduces the amount of data retrieved when requesting location data

--- crud/serializers.py
+++ crud/serializers.py
@@ -334,9 +334,8 @@ class TimeseriesWithFiltersSerializer(TimeseriesSerializerBase):
 
     """
     header = serializers.SerializerMethodField('get_header')
-    thresholdValueSets = ThresholdValueSetSerializer(read_only=True, many=True)
     filter_set = FilterSerializer(read_only=True, many=True)
 
     class Meta:
         model = Timeseries
-        fields = ('id', 'header', 'thresholdValueSets', 'filter_set')
+        fields = ('id', 'header', 'filter_set')
